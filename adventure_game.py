7895abf99a4c00dd9e137c0831dd3952b1186f39
Modebe Stephen <modebestephen2@gmail.com>
2022-04-28 19:33:20 +0100

    Add intro_function
    This function runs at the start of the game. Serves as game starting point.

--- adventure_game.py
+++ adventure_game.py
@@ -5,3 +5,25 @@ import random
 def print_pause(message_to_print):
     print(message_to_print)
     time.sleep(2)
+
+
+def intro1():
+    print_pause("You find yourself standing in an open field,"
+                "filled with grass and yellow wildflowers.")
+    print_pause(f"Rumor has it that a dragon "
+                "is somewhere around here,"
+                "and has been terrifying the nearby village.")
+    print_pause("In front of you is a house.")
+    print_pause("To your right is a dark cave.")
+    print_pause(f"In your hand you hold your "
+                f"rusty dagger.\n")
+
+
+def intro2():
+    print_pause("Enter 1 to knock on the door of the house.")
+    print_pause("Enter 2 to peer into the cave.")
+
+
+def intro():
+    intro1()
+    intro2()
